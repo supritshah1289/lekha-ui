e7d00a983b99056870c1eb5e5ba88832bb18b9df
suprit <suprit7@gmail.com>
2024-05-16 21:04:03 -0400

    update redirect url

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,11 @@
 export const API_BASE_URL = "https://supritshah.com";
 export const ACCESS_TOKEN = "accessToken";
 
-export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
+// export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
+export const OAUTH2_REDIRECT_URI =
+  "https://lekha-ui.vercel.app/oauth2/redirect";
+
+// https://lekha-ui.vercel.app/
 
 export const GOOGLE_AUTH_URL =
   API_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
